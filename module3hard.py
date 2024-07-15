8d7c7f6b5722e5265104652208dd2e8406968dca
IngaKon <kononova.inga@list.ru>
2024-07-15 19:52:20 +0300

    Add files via upload

---
+++ module3hard.py
@@ -0,0 +1,28 @@
+data_structure = [
+  [1, 2, 3],
+  {'a': 4, 'b': 5},
+  (6, {'cube': 7, 'drum': 8}),
+  "Hello",
+  ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+def calculate_structure_sum (data_structure):
+    counter = 0
+    if isinstance(data_structure, dict):
+        for key, value in data_structure.items():
+            counter += calculate_structure_sum(key)
+            counter += calculate_structure_sum(value)
+
+    elif isinstance(data_structure, (list, tuple, set)):
+        for item in data_structure:
+            counter += calculate_structure_sum(item)
+
+    elif isinstance(data_structure, (int, float)):
+        counter += data_structure
+
+    elif isinstance(data_structure, str):
+                return len(data_structure)
+    return counter
+
+result = calculate_structure_sum(data_structure)
+print(result)
